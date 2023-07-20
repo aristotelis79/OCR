ee409515d4d170044bfc292bfa494641074675a5
aristotelis <aristotelis79@gmail.com>
2023-07-20 07:54:53 +0300

    start of qrcode

--- src/ocr/home/views.py
+++ src/ocr/home/views.py
@@ -4,29 +4,41 @@ import numpy
 import bardapi
 import re
 import os 
+from qreader import QReader 
 
-from PIL import Image
+#from PIL import Image
 from django.shortcuts import render
-from django.http import HttpResponse
+
+def qrcode(request):
+    if request.method != 'POST' or request.FILES.get('invoice', None) is None :
+        return render(request, "home/app.html", {'json_data': ''})
+
+    qreader = QReader()
+    im = cv2.imdecode(numpy.fromstring(request.FILES['invoice'].read(), numpy.uint8), cv2.IMREAD_UNCHANGED)
+    image = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    decoded_code = qreader.detect_and_decode(image=image)
+
+    return render(request, "home/app.html", {'json_data': decoded_code})
 
 def home(request):
-    if request.method == 'POST' and request.FILES['invoice']:
-        #img = Image.open(request.FILES['invoice']) 
-        img = cv2.imdecode(numpy.fromstring(request.FILES['invoice'].read(), numpy.uint8), cv2.IMREAD_GRAYSCALE)
-        text = pytesseract.image_to_string(img, config= r'-l ell+eng --psm 6')
-        
-        promt_keys = request.POST.get('promt_keys')
-        extra_promts = request.POST.get('extra_promts')
-        if(extra_promts):
-            promt_keys += ","+ extra_promts 
-
-        promt = request.POST.get('promt').format(promt_keys)
-        promt += "'" + text + "'"
-        response = bardapi.core.Bard(os.environ.get('BARD1PSID')).get_answer(promt)
-
-        return render(request, "home/app.html", {'json_data': getJson(response['content'])})
+    if request.method != 'POST' or request.FILES.get('invoice', None) is None :
+        return render(request, "home/app.html", {'json_data': 'Not valid request'})
     
-    return render(request, "home/app.html", {'json_data': ''})
+    #img = Image.open(request.FILES['invoice']) 
+    image = cv2.imdecode(numpy.fromstring(request.FILES['invoice'].read(), numpy.uint8), cv2.IMREAD_GRAYSCALE)
+    text = pytesseract.image_to_string(image, config= r'-l ell+eng --psm 6')
+        
+    promt_keys = request.POST.get('promt_keys')
+    extra_promts = request.POST.get('extra_promts')
+    if(extra_promts):
+        promt_keys += ","+ extra_promts 
+
+    promt = request.POST.get('promt').format(promt_keys)
+    promt += "'" + text + "'"
+    response = bardapi.core.Bard(os.environ.get('BARD1PSID')).get_answer(promt)
+
+    return render(request, "home/app.html", {'json_data': getJson(response['content'])})
+    
 
 def getJson(input):
     pattern = r'```(.*?)```'
--- src/ocr/urls.py
+++ src/ocr/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ocr.home.views import home
+from ocr.home.views import home, qrcode
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home')
+    #path('', home, name='home')
+    path('', qrcode, name='home')
 ]
